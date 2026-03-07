9d850bc4015dbfc4493dd78b427daedf17e6c692
Mikah <annelayno25@gmail.com>
2026-03-07 13:33:37 +0800

    feat: add initial database configuration and connection setup.

--- api/db_config.php
+++ api/db_config.php
@@ -4,7 +4,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-$servername = "localhost:3308";
+$servername = "localhost";
 $dbusername = "mbc-smdi";
 $dbpassword = "mbccreations0331";
 $dbname = "smdi_website_db";
